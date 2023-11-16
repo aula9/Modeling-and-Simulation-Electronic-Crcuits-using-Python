fb424ad864acaad8b33b3fc3d12ab67824c641b2
Aula Jazmati <37100066+aula9@users.noreply.github.com>
2023-11-16 08:43:52 -0800

    Update ClipperCircuit.py

--- ClipperCircuit.py
+++ ClipperCircuit.py
@@ -1,18 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def diode_clipper(input_signal, V_clip):
+def diode_clipper(input_signal, V_clip, Vk):
     output_signal = input_signal.copy()
     for i in range(len(output_signal)):
-        if output_signal[i] > V_clip:
-            output_signal[i] = V_clip
-        elif output_signal[i] < -V_clip:
-            output_signal[i] = -V_clip
+        if output_signal[i] > V_clip+Vk:
+            output_signal[i] = V_clip+Vk
+        elif output_signal[i] < -V_clip -Vk:
+            output_signal[i] = -V_clip -Vk
     return output_signal
 
 t = np.linspace(0, 2*np.pi, 1000)  
 v_in = 10*np.sin(t)  
-v_out = diode_clipper(v_in, 2) 
+v_out = diode_clipper(v_in, 3,0.7) 
 
 plt.plot(t, v_in, label='Input Signal')
 plt.plot(t, v_out, label='Output Signal')
